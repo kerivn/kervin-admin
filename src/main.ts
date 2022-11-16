e15d3f94bca6516885d2267967f76a8208e82eb6
kervin <kervin_s@outlook.com>
2022-11-16 16:10:57 +0800

    feat(custom): change favicon

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-import store from './store'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import store from './store';
 import router from '@/router';
- // 创建vue实例
- const app = createApp(App)
- ​
 
-app.use(store)
-app.use(router)
- ​
- // 挂载实例
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
 app.mount('#app');
